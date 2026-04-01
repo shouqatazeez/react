00ad3f6409614dee37dde92aa9e7b275ba7d0bd7
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-04-02 00:11:40 +0530

    decalred values in app

--- Contact/src/App.jsx
+++ Contact/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Props from './props'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>App
+      <Props name="shouqat" course="B.tech" />
+      <Props name="Ahmad" courst="Engineering" />
+    </div>
   )
 }
 
