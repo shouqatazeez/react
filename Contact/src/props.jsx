eb17160c29843c2ce1eeaafd7f0a71fb87acfc2f
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-04-02 00:11:26 +0530

    add props file

---
+++ Contact/src/props.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Props = (props) => {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.course}</p>
+        </div>
+    )
+}
+
+export default Props;
