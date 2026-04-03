6d19988b802f2ee4d8e0a6db76d155ce154b8216
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-04-03 21:07:39 +0530

    work with usestate hook

--- Contact/src/App.jsx
+++ Contact/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Props from './props'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [likes, setlikes] = useState(0)
+
+
   return (
-    <div>App
+    <div>
+
       <Props name="shouqat" course="B.tech" />
       <Props name="Ahmad" course="Engineering" />
+      <p>Likes :{likes}</p>
+      <button onClick={() => setlikes(likes + 1)}>Click to Like</button>
     </div>
   )
 }
